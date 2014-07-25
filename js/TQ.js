c795f0422cde9f01d2973fb3e36a655a543dc3a8
Rance <Rance@rdy2.com>
2014-07-25 09:21:05 +0800

    试图修正 加载时图片大小闪烁bug 修改 使用seajs 载入 TQ

---
+++ js/TQ.js
@@ -0,0 +1,6 @@
+define(function(require){
+  require.async("http://float2006.tq.cn/floatcard?adminid=9428013&sort=0", function(){
+    require.async("http://sysimages.tq.cn/js/vip/all_20100501.js");
+  });
+});
+
